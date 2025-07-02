f4ed8cc8105e000163841a328e54be20134070f8
dacasfilipe <ezequiasmoreira@hotmail.com>
2025-07-01 21:24:49 -0300

    feat: integrate latest invoices component and fetch revenue data

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,11 @@
 import {lusitana} from "@/app/ui/fonts"
 import RevenueChart from "../ui/dashboard/revenue-chart"
-import { revenue } from "../lib/placeholder-data"
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
 export default async function Page(){
+    const revenue = await fetchRevenue(); //busca as receitas
+    const latestInvoices = await fetchLatestInvoices();
+    //buscas as últimas faturas
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl 
@@ -13,6 +17,7 @@ export default async function Page(){
             </div>
             <div className="mt-6 grid gap-6 sm:grid-cols-2 lg:grid-cols-8">
                 <RevenueChart revenue={revenue} />
+                <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
     )
